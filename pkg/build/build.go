a3b30c3d9e3bb0b4b9f0205f96e7647b8be4f349
Tomi Amao <tomi@amao.io>
2024-05-08 15:30:29 +0100

    added react builder

--- pkg/build/build.go
+++ pkg/build/build.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 
 	"github.com/simplifyd-systems/buildman/pkg/builder/next"
+	"github.com/simplifyd-systems/buildman/pkg/builder/react"
 	"github.com/simplifyd-systems/buildman/pkg/detect"
 )
 
 func Build(dir string, framework detect.Framework) (string, error) {
 	if framework == detect.NextFramework {
 		return next.Build(dir)
+	} else if framework == detect.ReactFramework {
+		return react.Build(dir)
 	}
 
 	return "", fmt.Errorf("cannot build the %s framework", framework)
